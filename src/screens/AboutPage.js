9ed91654594e69e94200fb0b1de4901f0ac7abfb
Charlie <charlyfinos@gmail.com>
2021-05-11 04:42:06 -0300

    styling about page

--- src/screens/AboutPage.js
+++ src/screens/AboutPage.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import avatar from '../img/avatar.png'
 
 const AboutPage = () => {
     return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}>
-            <h1>About</h1>
-        </motion.div>
+        <div >
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="about-page">
+                <div className="about-image">
+                    <img src={avatar}>
+                    </img>
+                </div>
+                <div className="about-text">
+                    <h1 className="hero-text">About Me:</h1>
+                    <p className="hero-sub-text">I'm Carlos. I am a react developer. I'm Carlos. I am a react developer. I'm Carlos. I am a react developer.I'm Carlos. I am a react developer.I'm Carlos. I am a react developer.I'm Carlos. I am a react developer.I'm Carlos. I am a react developer.</p>
+                </div>
+
+
+            </motion.div>
+        </div>
     )
 }
 
