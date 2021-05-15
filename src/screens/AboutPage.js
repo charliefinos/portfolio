9e15e24f6b119b4c61be765d14decfce0efd70ca
Charlie <charlyfinos@gmail.com>
2021-05-15 11:40:01 -0300

    create skills component

--- src/screens/AboutPage.js
+++ src/screens/AboutPage.js
@@ -1,25 +1,28 @@
 import React from 'react'
+import Skills from '../components/Skills'
 import { motion } from 'framer-motion'
 import avatar from '../img/avatar.png'
 
 const AboutPage = () => {
     return (
-        <div >
+        <div className="about">
             <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
-                className="about-page">
-                <div className="about-image">
-                    <img src={avatar}>
-                    </img>
+            >
+                <div className="about-page">
+                    <div className="about-image">
+                        <img src={avatar}>
+                        </img>
+                    </div>
+                    <div className="about-text">
+                        <h1 className="hero-text">About Me:</h1>
+                        <p className="hero-sub-text">Hi, I am Carlos Finos from Chaco, Argentina.</p>
+                        <p>I am a React Developer who learns and code everyday</p>
+                    </div>
                 </div>
-                <div className="about-text">
-                    <h1 className="hero-text">About Me:</h1>
-                    <p className="hero-sub-text">I'm Carlos. I am a react developer. I'm Carlos. I am a react developer. I'm Carlos. I am a react developer.I'm Carlos. I am a react developer.I'm Carlos. I am a react developer.I'm Carlos. I am a react developer.I'm Carlos. I am a react developer.</p>
-                </div>
-
-
+                <Skills />
             </motion.div>
         </div>
     )
