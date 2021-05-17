e9e1958c4cd43c5195686c376a1364001054f1b4
Charlie <charlyfinos@gmail.com>
2021-05-16 23:44:47 -0300

    styling portfolio

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { faGithub, faJs, faNodeJs } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Skills = () => {
 
     return (
-        <div>
-            <h1>Skills</h1>
+        <div className="skills">
+            <div className="skills-title">
+                <h1>Skills</h1>
+            </div>
+            <div className="skills-icons">
+                <FontAwesomeIcon icon={faGithub} className="icon-holder" />
+                <FontAwesomeIcon icon={faJs} className="icon-holder" />
+                <FontAwesomeIcon icon={faNodeJs} className="icon-holder" />
+                <FontAwesomeIcon icon={faGithub} className="icon-holder" />
+                <FontAwesomeIcon icon={faGithub} className="icon-holder" />
+                <FontAwesomeIcon icon={faGithub} className="icon-holder" />
+                <FontAwesomeIcon icon={faGithub} className="icon-holder" />
+                <FontAwesomeIcon icon={faGithub} className="icon-holder" />
+                <FontAwesomeIcon icon={faGithub} className="icon-holder" />
+                <FontAwesomeIcon icon={faGithub} className="icon-holder" />
+                <FontAwesomeIcon icon={faGithub} className="icon-holder" />
+                <FontAwesomeIcon icon={faGithub} className="icon-holder" />
+            </div>
         </div>
     )
 }
